af4cfcdb9abd7c24f30aa2fc3c73e69abb4316bc
Joe Hebert <raphaelhebert18@gmail.com>
2021-11-22 14:22:45 +0100

    imported react-router-dom and set up router and route to landing page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,23 @@
+import {
+  Route,
+  Switch,
+} from 'react-router-dom';
+
 import './App.css';
+
+import landing from "./components/landing";
+
 
 function App() {
   return (
+    <>
+    <Switch>
+      <Route path="/" render={landing} />
+    </Switch>
     <div className="App">
       Hello! this is Raphael...
     </div>
+    </>
   );
 }
 
