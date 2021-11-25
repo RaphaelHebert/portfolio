bb5cab61a88964b168b368a1c67ba481066d9c21
Joe Hebert <raphaelhebert18@gmail.com>
2021-11-25 21:22:22 +0100

    styled Cover

--- portfolio/src/components/landing/Cover.js
+++ portfolio/src/components/landing/Cover.js
@@ -3,12 +3,13 @@ import { useState } from "react";
 import "./landing.css"
 
 const Cover = () => {
-    const [mainClass, setMainClass] = useState("background")
+    const [mainClass, setMainClass] = useState("parentLanding")
     
     return(
         <div className={mainClass}>
+            <button className="enterButton" onClick={() => {setMainClass("disappear")}}>Come in!</button>
             <h1 className="title">Welcome !</h1>
-            <button className="button" onClick={() => setMainClass("background disappear")}>Come in!</button>
+            <div className="childLanding" />
         </div> 
     )
 }
--- portfolio/src/components/landing/index.js
+++ portfolio/src/components/landing/index.js
@@ -1,22 +1,12 @@
 import Cover from "./Cover";
 import Projects from "../projects";
 
-const handleClick = () => {
-    //Used for test purpose only
-    console.log("clicked !!!")
-}
-
 
 const Landing = () => {
     
     return (
-        
         <>
             <Cover />
-            <Projects/>
-            <div>
-                <button onClick={handleClick}>click me please..</button>
-            </div>
         </>
     )
 }
