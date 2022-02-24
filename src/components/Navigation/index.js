a7d5f731465b995dfbebd43c3959ed685bf92bf9
Joe Hebert <raphaelhebert18@gmail.com>
2022-02-24 13:11:06 +0700

    upgraded button navbar style

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,62 +1,27 @@
-import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import ProjectsNavigation from "./ProjectsNavigation";
+import ContactsNavigation from "./ContactsNavigation";
 
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import "./navigation.css"
 import "../../app.css"
 
-const Navigation = () => {
 
-  const navigate = useNavigate()
 
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+const NavigationBar = () => {
+    const navigate = useNavigate()
+    const handleClick = () => {
+        navigate('/', {replace: true})
+    }
 
-  const handleClose = (event) => {
-    setAnchorEl(null);
-  };
-
-  const nav = (event) => {
-    event.target.id? navigate(`/projects/${event.target.id}`, {replace: true}): navigate('/', {replace: true})
-  }
-
-  return (
-    <div className="main">
-      <Button
-        className="navButton"
-        id="basic-button"
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}
-      >
-        Projects
-      </Button>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        onClick={nav}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-      >
-        <MenuItem id="" className="customItem" onClick={handleClose}>Home</MenuItem>
-        <MenuItem id="NSDPY" className="customItem" onClick={handleClose}>PiPy Package</MenuItem>
-        <MenuItem id="TamGiang" className="customItem" onClick={handleClose}>Data science</MenuItem>
-        <MenuItem id="CodePen" className="customItem" onClick={handleClose}>CodePens</MenuItem>
-        <MenuItem id="flask" className="customItem" onClick={handleClose}>Web Dev - Flask</MenuItem>
-        <MenuItem id="react" className="customItem" onClick={handleClose}>Web Dev - React</MenuItem>
-      </Menu>
-    </div>
-  );
+    return(
+        <div className="flex rowNoWrap justifySpaceBetween navBar">
+            <div className="flex rowNoWrap justifySpaceAround">
+                <button className="navButton" id="basic-button" onClick={handleClick}>Home</button>
+                <ProjectsNavigation />
+            </div>
+            <ContactsNavigation />
+        </div>
+    )
 }
 
-export default Navigation
+export default NavigationBar;
